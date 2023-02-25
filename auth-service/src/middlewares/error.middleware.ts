620b508ad3c933082366e8fee19741fdd5cb819f
Nizhgo <8900800700a@gmail.com>
2023-02-25 18:17:01 +0300

    add error.middleware

---
+++ auth-service/src/middlewares/error.middleware.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express';
+
+/** Catch all unhandled errors and return a 500 response
+ * @param err {Error} - The error object
+ * @param req {Request} - The request object
+ * @param res {Response} - The response object
+ * @param next {NextFunction} - The next middleware function
+ * @description This middleware catches all unhandled errors and returns a 500 response with a generic error message. Save server from crashing.
+ * @returns {void}
+ */
+export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+	console.error(err.stack);
+	res.status(500).json({ error: 'Internal Server Error' });
+}
--- auth-service/src/routes/auth.routes.ts
+++ auth-service/src/routes/auth.routes.ts
@@ -5,20 +5,21 @@ import {RegistrationController} from "../controllers/registration.controller";
 import {TokenController} from "../controllers/token.controller";
 import {UserController} from "../controllers/user.controller";
 import {authMiddleware} from "../middlewares/auth";
+import {errorHandler} from "../middlewares/error.middleware";
 
 const router = express.Router();
 
-router.post('/login', LoginControllers.login);
-router.post('/register', RegistrationController.registerUser);
-router.get('/ping', (req, res) => res.status(200).json({message: "pong"}));
+router.post('/login',  LoginControllers.login, errorHandler);
+router.post('/register', errorHandler,  RegistrationController.registerUser, errorHandler);
+router.get('/ping',  (req, res) => res.status(200).json({message: "pong"}, errorHandler));
 //protected routes
 
-router.post('/disableToken', authMiddleware, TokenController.disableToken);
-router.post('/logout', authMiddleware, LoginControllers.logout);
-router.get('/refresh-token', TokenController.validateRefreshToken, TokenController.reissueAccessToken);
-router.post('/changePassword', TokenController.validateAccessToken, PasswordController.changePassword);
-router.get('/user', authMiddleware, UserController.getUser);
-router.get('/active-tokens', authMiddleware, UserController.getActiveRefreshTokens);
-router.get('/auth-ping', authMiddleware, (req, res) => res.status(200).json({message: "pong"}));
+router.post('/disableToken', authMiddleware, TokenController.disableToken, errorHandler);
+router.post('/logout', authMiddleware, LoginControllers.logout, errorHandler);
+router.get('/refresh-token', TokenController.validateRefreshToken, TokenController.reissueAccessToken, errorHandler);
+router.post('/changePassword', TokenController.validateAccessToken, PasswordController.changePassword, errorHandler);
+router.get('/user', authMiddleware, UserController.getUser, errorHandler);
+router.get('/active-tokens', authMiddleware, UserController.getActiveRefreshTokens, errorHandler);
+router.get('/auth-ping', authMiddleware, (req, res) => res.status(200).json({message: "pong"}, errorHandler));
 
 export default router;
